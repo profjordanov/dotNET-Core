8b0224fac97ea646c96f2ec1db5f2063caf38879
Jordan Jordanov <d_yugioh@abv.bg>
2018-02-07 23:23:57 -0800

    3

---
+++ Basic Web App with ASP.NET Core, MVC, Entity Framework Core, Bootstrap, and Angular/DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+  var x = 0;
+  var s = "";
+
+  console.log("Hello Pluralsight");
+
+
+
+  var theForm = $("#theForm");
+  theForm.hide();
+
+  var button = $("#buyButton");
+  button.on("click", function () {
+    console.log("Buying Item");
+  });
+
+  var productInfo = $(".product-props li");
+  productInfo.on("click", function () {
+    console.log("clicked one of the items: " + $(this).text());
+  });
+
+  var $loginToggle = $("#loginToggle");
+  var $popupForm = $(".popup-form");
+
+  $loginToggle.on("click", function () {
+    $popupForm.fadeToggle(1000);
+  });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
