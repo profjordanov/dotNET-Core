bf6a3307d44927833854a3afdc1151f31612ad19
Jordan Jordanov <d_yugioh@abv.bg>
2018-02-07 23:30:46 -0800

    04

---
+++ Basic Web App with ASP.NET Core, MVC, Entity Framework Core, Bootstrap, and Angular/4/DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+  public class AppController : Controller
+  {
+    private readonly IMailService _mailService;
+
+    public AppController(IMailService mailService)
+    {
+      _mailService = mailService;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpGet("contact")]
+    public IActionResult Contact()
+    {
+      return View();
+    }
+
+    [HttpPost("contact")]
+    public IActionResult Contact(ContactViewModel model)
+    {
+      if (ModelState.IsValid)
+      {
+        // Send the email
+        _mailService.SendMessage("shawn@wildermuth.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+        ViewBag.UserMessage = "Mail Sent";
+        ModelState.Clear();
+      }
+
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      ViewBag.Title = "About Us";
+
+      return View();
+    }
+
+  }
+}
---
+++ Basic Web App with ASP.NET Core, MVC, Entity Framework Core, Bootstrap, and Angular/4/DutchTreat/DutchTreat/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DutchTreat
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddTransient<IMailService, NullMailService>();
+      // Support for real mail service
+
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseMvc(cfg =>
+      {
+        cfg.MapRoute("Default", 
+          "{controller}/{action}/{id?}", 
+          new { controller = "App", Action = "Index" });
+      });
+
+    }
+  }
+}
