e2cf19914e1715aeb0d7f69583d43a9348e12510
Jordan Jordanov <d_yugioh@abv.bg>
2018-02-09 08:57:31 +0200

    5

---
+++ Basic Web App with ASP.NET Core, MVC, Entity Framework Core, Bootstrap, and Angular/5/DutchTreat/DutchTreat/ViewModels/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutchTreat.ViewModels
+{
+  public class ContactViewModel
+  {
+    [Required]
+    [MinLength(5)]
+    public string Name { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string Subject { get; set; }
+    [Required]
+    [MaxLength(250, ErrorMessage = "Too Long")]
+    public string Message { get; set; }
+  }
+}
