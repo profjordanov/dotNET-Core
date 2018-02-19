f3605c8ead643414b08f567da85dbe9ef7623df0
Jordan Jordanov <d_yugioh@abv.bg>
2018-02-19 11:33:56 +0200

    02

---
+++ Implementing and Securing an API with ASP.NET Core/02/MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
+
+namespace MyCodeCamp.Controllers
+{
+  [Route("api/[controller]")]
+  public class CampsController : Controller
+  {
+    private ICampRepository _repo;
+
+    public CampsController(ICampRepository repo)
+    {
+      _repo = repo;
+    }
+
+    [HttpGet("")]
+    public IActionResult Get()
+    {
+      var camps = _repo.GetAllCamps();
+
+      return Ok(camps);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id, bool includeSpeakers = false)
+    {
+      try
+      {
+        Camp camp = null;
+
+        if (includeSpeakers) camp = _repo.GetCampWithSpeakers(id);
+        else camp = _repo.GetCamp(id);
+
+        if (camp == null) return NotFound($"Camp {id} was not found");
+
+        return Ok(camp);
+      }
+      catch
+      {
+      }
+
+      return BadRequest();
+    }
+
+  }
+}
+
+
+
+
+
+
