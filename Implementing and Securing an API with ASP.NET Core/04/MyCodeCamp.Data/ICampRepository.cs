3ed88608ec952c02cd76c1d5af7d34005a6fe8fd
Jordan Jordanov <d_yugioh@abv.bg>
2018-02-21 08:53:18 +0200

    04

---
+++ Implementing and Securing an API with ASP.NET Core/04/MyCodeCamp.Data/ICampRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyCodeCamp.Data.Entities;
+
+namespace MyCodeCamp.Data
+{
+  public interface ICampRepository
+  {
+    // Basic DB Operations
+    void Add<T>(T entity) where T : class;
+    void Delete<T>(T entity) where T : class;
+    Task<bool> SaveAllAsync();
+
+    // Camps
+    IEnumerable<Camp> GetAllCamps();
+    Camp GetCamp(int id);
+    Camp GetCampWithSpeakers(int id);
+    Camp GetCampByMoniker(string moniker);
+    Camp GetCampByMonikerWithSpeakers(string moniker);
+
+    // Speakers
+    IEnumerable<Speaker> GetSpeakers(int id);
+    IEnumerable<Speaker> GetSpeakersWithTalks(int id);
+    IEnumerable<Speaker> GetSpeakersByMoniker(string moniker);
+    IEnumerable<Speaker> GetSpeakersByMonikerWithTalks(string moniker);
+    Speaker GetSpeaker(int speakerId);
+    Speaker GetSpeakerWithTalks(int speakerId);
+
+    // Talks
+    IEnumerable<Talk> GetTalks(int speakerId);
+    Talk GetTalk(int talkId);
+
+    // CampUser
+    CampUser GetUser(string userName);
+  }
+}
---
+++ Implementing and Securing an API with ASP.NET Core/04/MyCodeCamp/Controllers/BaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace MyCodeCamp.Controllers
+{
+  public abstract class BaseController : Controller
+  {
+    public const string URLHELPER = "URLHELPER";
+
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+      base.OnActionExecuting(context);
+      context.HttpContext.Items[URLHELPER] = this.Url;
+    }
+  }
+}
