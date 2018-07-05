6d0d5c0e00a40a6921d0c82b2e8cc9eb6ac893e7
Jordan Jordanov <d_yugioh@abv.bg>
2018-07-05 08:58:51 +0300

    Add files via upload

---
+++ TodoApp/TodoApp.Shared/ToDoList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Shared
+{
+    public class ToDoList
+    {
+        public int Id { get; set; }
+        public string Item { get; set; }
+    }
+}
