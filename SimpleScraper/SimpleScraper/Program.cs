ad9500073c8548974bbf5194a439d493e819bea1
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-14 09:04:52 +0200

    Add files via upload

---
+++ SimpleScraper/SimpleScraper/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimpleScraper
+{
+	internal class Program
+    {
+	    private static async Task Main(string[] args)
+        {
+	        var url = Console.ReadLine();
+	        using (var httpClient = new HttpClient())
+	        {
+		        var output = await httpClient.GetStringAsync(url);
+				Console.WriteLine(output);
+	        }
+        }
+    }
+}
