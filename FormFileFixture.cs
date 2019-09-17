f82144e08eaead45fb70096e6eb252bfc0bbe885
Jordan Jordanov <d_yugioh@abv.bg>
2019-09-17 14:03:43 +0300

    Create FormFileFixture.cs

---
+++ FormFileFixture.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace HeidelbergCement.Platform.Downloads.Api.Test.Helpers
+{
+    public static class FormFileFixture
+    {
+        public static IFormFile CreateFormFileByEmbeddedResource(string resourceName, string fileName)
+        {
+            var file = new Mock<IFormFile>();
+
+            var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+
+            var memoryStream = new MemoryStream();
+            var streamWriter = new StreamWriter(memoryStream);
+
+            streamWriter.Write(resourceStream);
+            streamWriter.Flush();
+
+            memoryStream.Position = 0;
+
+            file.Setup(f => f.FileName).Returns(fileName).Verifiable();
+
+            file.Setup(f => f.Length).Returns(memoryStream.Length);
+
+            file.Setup(_ => _.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+                .Returns((Stream stream, CancellationToken token) => memoryStream.CopyToAsync(stream, token))
+                .Verifiable();
+
+            file.Setup(formFile => formFile.OpenReadStream())
+                .Returns(resourceStream)
+                .Verifiable();
+
+            return file.Object;
+        }
+    }
+}
