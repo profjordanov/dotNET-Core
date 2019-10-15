f8f572086afef30b3162fa85f44a172007a072af
Jordan Jordanov <d_yugioh@abv.bg>
2019-10-15 12:21:36 +0300

    Hello Executables

---
+++ dotNet Core 3.0/HelloExecutables/HelloExecutables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloExecutables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What version am I? " + Environment.Version);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
