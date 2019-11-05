74d3deffe92a8de8c5f8e20f339228223eea82bc
Jordan Jordanov <d_yugioh@abv.bg>
2019-11-05 10:40:08 +0200

    Add files via upload

---
+++ QueryStringObject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Dynamic;
+using System.Threading.Tasks;
+
+namespace QueryStringObject
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            dynamic ds = new ExpandoObject();
+            ds.countryId = "US";
+            ds.orgUnitId = "1316464";
+            ds.expiryDate = "*";
+
+            var queryManager = new QueryManager();
+            var queryString = await queryManager.ToQueryStringAsync(ds);
+
+            Console.WriteLine("string");
+            Console.WriteLine(queryString);
+        }
+    }
+}
---
+++ QueryStringObject/UrlHelpers.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace QueryStringObject
+{
+    public class QueryManager
+    {
+        public async Task<string> ToQueryStringAsync(object obj)
+        {
+            var innerObj = new
+            {
+                scope = obj
+            };
+
+            var keyValueContent = innerObj.ToKeyValue();
+            var formUrlEncodedContent = new FormUrlEncodedContent(keyValueContent);
+            var urlEncodedString = await formUrlEncodedContent.ReadAsStringAsync();
+            return urlEncodedString;
+        }
+    }
+
+    public static class UrlHelpers
+    {
+        public static IDictionary<string, string> ToKeyValue(this object metaToken)
+        {
+            if (metaToken == null)
+            {
+                return null;
+            }
+
+            if (!(metaToken is JToken token))
+            {
+                return ToKeyValue(JObject.FromObject(metaToken));
+            }
+
+            if (token.HasValues)
+            {
+                var contentData = new Dictionary<string, string>();
+                foreach (var child in token.Children().ToList())
+                {
+                    var childContent = child.ToKeyValue();
+                    if (childContent != null)
+                    {
+                        contentData = contentData.Concat(childContent)
+                            .ToDictionary(k => k.Key, v => v.Value);
+                    }
+                }
+
+                return contentData;
+            }
+
+            var jValue = token as JValue;
+
+            if (jValue?.Value == null)
+            {
+                return null;
+            }
+
+            var value = jValue?.Type == JTokenType.Date ?
+                jValue?.ToString("o", CultureInfo.InvariantCulture) :
+                jValue?.ToString(CultureInfo.InvariantCulture);
+
+            return new Dictionary<string, string>
+            {
+                { token.Path, value }
+            };
+        }
+    }
+}
